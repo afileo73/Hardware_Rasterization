78afc6457534a01c62098c4bab52830d4e3a3042
Alex Fillmore <afillmore@oakland.edu>
2024-01-25 14:18:15 -0500

    Added component schematics + register linting

--- Source/Loader/NReg.vhd
+++ Source/Loader/NReg.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+
 entity NReg is
 	 generic(N:integer := 8);
 	 port(
@@ -10,17 +11,21 @@ entity NReg is
 		 q : out STD_LOGIC_VECTOR(N-1 downto 0)
 	     );
 end NReg;
+
 architecture NReg of NReg is
+
 begin
+
 	process(clk, clr)
 	begin
 		if clr = '1' then
 			q <= (others => '0');
-		elsif clk'event and clk = '1' then
+		elsif rising_edge(clk) then
 			if load = '1' then
 				q <= d;
-			end if;	
-	end if;							 
+			end if;
+		end if;
 	end process;
+
 end NReg;
 
